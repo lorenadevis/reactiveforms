397ce4b806a45d56716bc3b0a125f1c2ee29df1b
Devis Gimeno, Lorena <lorena.devis@wolterskluwer.com>
2022-06-15 17:07:14 +0200

    reactive forms ejemplo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { FormularioBasicoComponent } from "./formulario-basico/formulario-basico.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  {
+    path: "contacto",
+    component: FormularioBasicoComponent
+  },
+  { path: "", redirectTo: "/contacto", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
