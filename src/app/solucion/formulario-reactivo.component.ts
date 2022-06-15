7025d40a94ef5db1e020436389221626a0c4ba6e
Devis Gimeno, Lorena <lorena.devis@wolterskluwer.com>
2022-06-15 18:23:06 +0200

    solucion sin validadores

--- src/app/solucion/formulario-reactivo.component.ts
+++ src/app/solucion/formulario-reactivo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-formulario-reactivo",
@@ -8,17 +8,14 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 })
 export class FormularioReactivoComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
+  public formulario: FormGroup;
 
-  formulario: FormGroup;
-
-  ngOnInit() {
+  public ngOnInit() {
     this.formulario = this.fb.group({
-      name: [
-        "Carlos",
-        [Validators.required, Validators.minLength(3), Validators.maxLength(8)]
-      ],
-      email: ["", [Validators.email, Validators.required]]
+      name: "Lorena",
+      email: ""
     });
+
   }
 
   onSubmit(_datos) {
